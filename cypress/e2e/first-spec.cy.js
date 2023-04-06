a304f80ad88cce1c4230121189a69911744edbf6
Josef Válek <valek.josef@seznam.cz>
2023-04-06 13:19:30 +0200

    Progress

--- cypress/e2e/first-spec.cy.js
+++
@@ -1,7 +0,0 @@
-describe('first tests suite', () => {
-    it('visit Apify main page', () => {
-        cy.visit('/');
-        cy.log('Visiting Apify main page');
-        cy.contains('div', 'Apify').should('exist');
-    });
-});
--- main.js
+++ main.js
@@ -1,62 +1,100 @@
 import { Actor } from 'apify';
 import fs from 'fs';
 import cypress from 'cypress';
-import { globby } from 'globby';
 import log from '@apify/log';
+import path from 'path';
 import 'console.table';
+import { fileURLToPath } from 'url';
+import { promisify } from 'util';
 
-// run tests from specific test file with given configuration
-const runOneSpec = (spec) => cypress.run({ config: input, spec });
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const fileStat = promisify(fs.stat);
+
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 await Actor.init();
 
-const input = await Actor.getInput();
-log.info(`Running tests with following input: ${JSON.stringify(input)}`);
+const { specCode, ...cypressConfig } = await Actor.getInput();
+log.info(`Running tests with following config: ${JSON.stringify({ specCode, ...cypressConfig })}`);
 
-const tests = await globby('./cypress/e2e/*-spec.cy.js');
-log.info(`Getting tests: ${tests}`);
+// TODO: Create this file from inut
+const specFilename = path.join(__dirname, 'cypress', 'e2e', 'spec.cy.js');
 
-const testsResultsSummary = [];
+await writeFile(specFilename, specCode, { encoding: 'utf-8' });
+
 const kvs = await Actor.openKeyValueStore();
 const dataset = await Actor.openDataset();
-for (const test of tests) {
-    const result = await runOneSpec(test);
-    let keyValueStoreLink;
-    if (result?.config?.video) {
-        const baseName = test.split('/').pop();
-        const file = `./cypress/videos/${baseName}.mp4`;
-        const kvsKeyName = baseName.replaceAll('.', '-');
-        await kvs.setValue(kvsKeyName, fs.readFileSync(file), { contentType: 'video/mp4' });
-        keyValueStoreLink = kvs.getPublicUrl(kvsKeyName);
-    }
 
-    const transformedResult = {
-        testSuiteTitle: result.runs[0].tests ? result.runs[0].tests[0].title[0] : result.runs[0].spec.baseName,
-        totalPassed: result.totalPassed,
-        totalPending: result.totalPending,
-        totalFailed: result.totalFailed,
-        totalSkipped: result.totalSkipped,
-        totalDuration: result.totalDuration,
-        videoLink: keyValueStoreLink,
-        rawData: result,
-    };
+const result = await cypress.run({ config: cypressConfig, spec: specFilename });
 
-    await dataset.pushData(transformedResult);
-    testsResultsSummary.push(transformedResult);
+// Save the full result as output
+await kvs.setValue('RESULT', result);
+
+// Save the HTML report as output
+const reportFilename = path.join(__dirname, 'cypress', 'reports', 'html', 'index.html');
+
+try {
+    await fileStat(reportFilename);
+} catch (e) {
+    log.error('Unable to generate report', { reportFilename });
+    Actor.exit({ exitCode: 1, statusMessage: 'Report file not found' });
 }
 
-const summary = testsResultsSummary
-    .map((test) => {
-        return {
-            spec: test?.testSuiteTitle,
-            passes: test?.totalPassed,
-            failures: test?.totalFailed,
-            pending: test?.totalPending,
-            skipped: test?.totalSkipped,
-            duration: test?.totalDuration,
-        };
-    });
+await kvs.setValue(
+    'OUTPUT',
+    await readFile(reportFilename, { encoding: 'utf-8' }),
+    { contentType: 'text/html' },
+);
 
-console.table('Summary', summary);
+const run = result.runs[0];
+
+if (run.video) {
+    await kvs.setValue('RECORDING', fs.readFileSync(run.video), { contentType: 'video/mp4' });
+}
+
+let screenshotCounter = 0;
+let allTestsPassed = true;
+
+// We want to push the tests to the dataset, not the specs - we only have one file to work with
+for (const test of run.tests) {
+    if (test.state !== 'passed') {
+        allTestsPassed = false;
+    }
+    // Duration is sum of duration of all attempts
+    let duration = 0;
+    const screenshotUrls = [];
+    for (const attempt of test.attempts) {
+        duration += attempt.duration;
+        // Save all the screenshots
+        for (const screenshot of attempt.screenshots) {
+            const screenshotName = `SCREENSHOT_${screenshotCounter}`;
+            const screenshotData = fs.readFileSync(screenshot.path);
+            await kvs.setValue(screenshotName, screenshotData, { contentType: 'image/png' });
+            screenshotUrls.push(kvs.getPublicUrl(screenshotName));
+            screenshotCounter++;
+        }
+    }
+    await dataset.pushData({
+        title: test.title.join(' '),
+        displayError: test.displayError,
+        state: test.state,
+        duration,
+        attempts: test.attempts.length,
+        screenshotUrl: screenshotUrls[0],
+        screenshotUrls,
+        rawData: test,
+    });
+}
+
+log.info('See dataset for results of individual tests', {
+    videoLink: run.video ? kvs.getPublicUrl('RECORDING') : undefined,
+    resultsLink: kvs.getPublicUrl('OUTPUT'),
+});
+
+if (!allTestsPassed) {
+    Actor.exit({ exitCode: 1, statusMessage: 'Some tests have failed' });
+}
 
 await Actor.exit();
